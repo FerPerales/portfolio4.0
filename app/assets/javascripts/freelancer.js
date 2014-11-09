9129707e188d64a6b0e95df9fe0710a65c1eff0f
Fer Perales <me@ferperales.net>
2014-11-09 15:59:21 -0600

    Remove unnecesary scripts

--- app/assets/javascripts/freelancer.js
+++ app/assets/javascripts/freelancer.js
@@ -4,17 +4,6 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
 // Floating label headings for the contact form
 $(function() {
     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
@@ -25,11 +14,6 @@ $(function() {
         $(this).removeClass("floating-label-form-group-with-focus");
     });
 });
-
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
